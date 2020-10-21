af8fdcef1ff6661f97de1c958bf6def47331d95b
Radhakrishnan R <rk@arkays.info>
2020-10-21 21:22:31 +0530

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { error } from 'protractor';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  // tslint:disable-next-line: typedef
+  login(){
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  logout() {
+    this.accountService.logout();
+  }
+
+  // tslint:disable-next-line: typedef
+
+
+}
