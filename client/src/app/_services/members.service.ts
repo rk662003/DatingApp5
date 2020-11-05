749cad36456bee64ca20cd47ca5c94579e8912b2
Radhakrishnan R <rk@arkays.info>
2020-11-05 21:38:27 +0530

    End of section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line: typedef
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
+
+  // tslint:disable-next-line: typedef
+  getMember(username: string){
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
